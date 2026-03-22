fb8957b06fb7a3877c137d1594b77397074290be
Pranav-Raj-31 <pranavraj3105@gmail.com>
2026-03-22 14:19:48 +0530

    changes

--- be/CRM.Api/Program.cs
+++ be/CRM.Api/Program.cs
@@ -89,6 +89,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.MapGet("/", () => Results.Redirect("/swagger")).ExcludeFromDescription();
 }
 
 app.UseCors();
